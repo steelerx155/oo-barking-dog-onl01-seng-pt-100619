9f8106c3b1c1f4e8fcb44db7b36a617a9372b235
Randy Steele <steelerx155@gmail.com>
2019-10-25 18:36:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,27 +11,4 @@ class Dog
     puts "woof!"
   end
 end
-
-# class Dog
-# def name=(dogs_name)  
-#   @the_dogs_name= dogs_name
-
-# def name
-#   @the_dogs_name 
-# end
-# end
-# end
- 
-# def bark
-#   puts "woof!"
-#   lassie = Dog.new
-# lassie.bark 
-
-# puts lassie.bark
- 
-
-# lassie = Dog.new
-# lassie.bark 
-
-# end
 # Your code goes here!
