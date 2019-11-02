2f4f6a907c8787ca61b07368b97741865bbeaa68
Randy Steele <steelerx155@gmail.com>
2019-11-02 18:33:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,32 @@
-class Dog
+# class Dog
+#   def name
+#     @name
+#   end
+
+#   def name=(dog_name)
+#     @name = dog_name
+#   end
+
+#   def bark
+#     puts "woof!"
+#   end
+# end
+# Your code goes here!
+
+
+
+class Dog 
+
   def name
-    @name
+  @name
+  end
+  
+  def name=(the_dogs_name)
+  @name = the_dogs_name
   end
 
-  def name=(dog_name)
-    @name = dog_name
-  end
-
-  def bark
-    puts "woof!"
+  def bark 
+  puts "woof!"
   end
 end
-# Your code goes here!
+  
