22c07542d1c5c4400cb278f8c12ab4eaeee321fe
Randy Steele <steelerx155@gmail.com>
2019-11-02 18:32:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,15 +17,15 @@
 
 class Dog 
 
+  def name
+  @name
+  end
+  
   def name=(the_dogs_name)
   this_dogs_name = the_dogs_name
   end
 
-  def name
-  @name
-  end
-
-  def bark 
+    def bark 
   puts "woof"
   end
 end
